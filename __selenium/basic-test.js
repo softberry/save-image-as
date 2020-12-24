7ebf46739218b3181ba6e5e66bd858c9441e61e3
softberry <emre.sakarya@softberry.de>
2020-12-24 15:50:17 +0100

    first selenium test successfully implemented

--- __selenium/basic-test.js
+++ __selenium/basic-test.js
@@ -1,32 +1,62 @@
-const webdriver = require("selenium-webdriver");
+const { resolve } = require("path");
+const { Builder, By } = require("selenium-webdriver");
+const { FileDetector } = require("selenium-webdriver/remote");
 
 const dotenv = require("dotenv");
 dotenv.config();
 
 USERNAME = process.env.BROWSERSTACK_USERNAME;
 AUTOMATE_KEY = process.env.BROWSERSTACK_ACCESS_KEY;
-const browserstackURL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+const browserstackURL =
+  "https://" +
+  USERNAME +
+  ":" +
+  AUTOMATE_KEY +
+  "@hub-cloud.browserstack.com/wd/hub";
 
 // Input capabilities
-var capabilities = {
-  os: "Windows",
+const capabilities = {
   os_version: "10",
-  browserName: "Chrome",
-  browser_version: "80",
-
-  name: "save-image-as: Basic Test",
+  resolution: "1920x1080",
+  browserName: "Firefox",
+  browser_version: "latest",
+  os: "Windows",
+  name: "BStack-[NodeJS] Sample Test", // test name
+  build: "BStack Build Number 1",
+  //   "browserstack.sendKeys": true,
+  "browserstack.debug": true,
+  "browserstack.networkLogs": true,
 };
 
-var driver = new webdriver.Builder().usingServer(browserstackURL).withCapabilities(capabilities).build();
+const driver = new Builder()
+  .forBrowser("chrome")
+  .usingServer(browserstackURL)
+  .withCapabilities(capabilities)
+  .build();
+driver.setFileDetector(new FileDetector());
 
-driver.get("http://www.google.com").then(function () {
-  driver
-    .findElement(webdriver.By.name("q"))
-    .sendKeys("BrowserStack")
-    .then(function () {
-      driver.getTitle().then(function (title) {
-        console.log(title);
-        driver.quit();
-      });
-    });
-});
+const basicTest = async () => {
+  try {
+    const mockFile = resolve(__dirname, "./color-palette.png");
+
+    await driver.get("https://softberry.github.io/save-image-as/");
+
+    const imageFile = await driver.findElement(By.id("imageFile"));
+    const d = await imageFile.isDisplayed();
+    const d1 = await imageFile.sendKeys(mockFile);
+    console.log("§§§§§", d, d1);
+    const resultImg = await driver.findElement(By.css("#result"));
+    const src = await resultImg.getAttribute("src");
+    if (src.startsWith("data:image/jpeg;base64")) {
+      console.log("Success");
+    } else {
+      console.log("Error");
+    }
+    (await driver).quit();
+  } catch (err) {
+    console.log(err);
+    (await driver).quit();
+  }
+};
+
+basicTest();
